1c606e1c1aedbcd2d62e3aaa89eb0e00eb689669
JuanCarlos92 <juancarlosfilter@gmail.com>
2024-10-30 11:57:55 +0100

    class24

--- Spring/springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ Spring/springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.juancarlos.curso.springboot.webapp.springboot_web.controllers;
 
+// import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.juancarlos.curso.springboot.webapp.springboot_web.models.User;
 import com.juancarlos.curso.springboot.webapp.springboot_web.models.dto.UserDto;
+
 
 @RestController
 @RequestMapping("/api")
@@ -25,6 +29,22 @@ public class UserRestController {
 
         return userDto;
     }
+    @GetMapping("/list")
+    public List<User> list() {
+        User user =  new User("juan carlos", "filter martin");
+        User user2 =  new User("juan manuel", "martin alvarez");
+        User user3 =  new User("rosa maria", "pradas martin");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
+
+    }
+    
 
     @GetMapping("/details-map")
     public Map<String, Object> detailsMap() {
--- Spring/springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/models/User.java
+++ Spring/springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/models/User.java
@@ -4,9 +4,18 @@ public class User {
 
     private String name;
     private String lastname;
+    private String email;
+
     
     public User() {
     }
+
+
+    public User(String name, String lastname, String email) {
+        this(name, lastname);
+        this.email = email;
+    }
+
 
     public User(String name, String lastname) {
         this.name = name;
@@ -27,6 +36,14 @@ public class User {
     
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
 
