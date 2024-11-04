6d5c65f4df19a99630deddc190a3007a3fa18269
JuanCarlos92 <juancarlosfilter@gmail.com>
2024-11-05 00:03:59 +0100

    springboot modelAttribute

--- springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.juancarlos.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.juancarlos.curso.springboot.webapp.springboot_web.models.User;
 
@@ -13,11 +18,28 @@ public class UserController {
     public String details(Model model) {
 
         User user = new User("JuanC", "Filter");
-
+        user.setEmail("correo@gmail.com");
         model.addAttribute("title", "Hola mundo spring boot");
         model.addAttribute("user", user);
         return "details";
 
     }
 
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+
+        model.addAttribute("title", "Listado de usuarios!");
+
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> userModel() {
+        return Arrays.asList(
+                new User("pepa", "Gonzalez"),
+                new User("Manuel", "Jimenez"),
+                new User("Alberto", "Hebles"),
+                new User("Felix", "Ruger"));
+
+    }
 }
--- springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/juancarlos/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,8 @@
 package com.juancarlos.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,21 +10,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.juancarlos.curso.springboot.webapp.springboot_web.models.User;
+import com.juancarlos.curso.springboot.webapp.springboot_web.models.dto.UserDto;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String, Object> details(){
-        Map <String, Object> body = new HashMap<>();
-        User user = new User("JuanC","Filter");
+    public UserDto details() {
+
+        User user = new User("JuanCarlos", "Filter");
+
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola mundo Spring Boot");
+
+        return userDto;
+
+    }
+
+    @GetMapping("/list")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+
+    public List<User> list() {
+        User user = new User("JuanCarlos", "Filter");
+        User user2 = new User("JuanMiguel", "Martin");
+        User user3 = new User("Jose", "Alvarez");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
+
+    }
+
+    @GetMapping("/details-map")
+    public Map<String, Object> detailsMap() {
+        Map<String, Object> body = new HashMap<>();
+        User user = new User("JuanC", "Filter");
 
         body.put("title", "Hola mundo spring boot");
         body.put("user", user);
-        
+
         return body;
-        
+
     }
 
 }
